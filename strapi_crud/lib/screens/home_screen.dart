8de03ee06438af1ba5b7a141a690ebf12fa6dd59
Mridul2820 <immridulpanda2820@gmail.com>
2022-02-18 00:44:44 +0530

    showing user details

--- strapi_crud/lib/screens/home_screen.dart
+++ strapi_crud/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/new_alarm_screen.dart';
 import '../components/title.dart';
 
 import '../components/screen_container.dart';
@@ -13,8 +14,29 @@ class HomeScreen extends StatelessWidget {
       SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: ScreenTitle(
-            'Hello ' + responseData['username'],
+          child: Column(
+            children: [
+              ScreenTitle(
+                'Hello ' + responseData['username'],
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.red,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return NewAlarmScreen();
+                      },
+                    ),
+                  );
+                },
+                child: const Icon(Icons.add_alarm),
+              ),
+            ],
           ),
         ),
       ),
---
+++ strapi_crud/lib/screens/new_alarm_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewAlarmScreen extends StatelessWidget {
+  const NewAlarmScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
